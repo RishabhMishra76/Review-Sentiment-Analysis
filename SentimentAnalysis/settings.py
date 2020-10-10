096baac29b75fd2aa0751b049962f56270daaf31
Rishabh Mishra <mishrarishabh76@gmail.com>
2020-10-10 12:03:49 +0530

    additional files and changes for Heroku

--- SentimentAnalysis/settings.py
+++ SentimentAnalysis/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'p2^bh&4k$ox2^s88*svdbvm6vml+kc8_!c%6=-p%07(bg_o@ck'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+#ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
@@ -83,7 +83,16 @@ WSGI_APPLICATION = 'SentimentAnalysis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'SentimentAnalysis',
@@ -92,7 +101,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
     }
-}
+}'''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
